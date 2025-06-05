dd9706ec34a68d1cda4edc10e43a1a955f73b6d5
DijanU <pad23663@uvg.edu.gt>
2025-06-05 00:52:48 -0600

    add: product api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,3 +10,11 @@ Route::get('/users/{id}',[UserController::class, 'show']);
 Route::post('/users',[UserController::class, 'store']);
 Route::put('/users/{id}',[UserController::class, 'update']);
 Route::delete('/users/{id}',[UserController::class, 'destroy']);
+
+/**Product routes */
+
+Route::apiResource('products', ProductController::class);
+Route::get('products/brand/{brandId}', [ProductController::class, 'getByBrand']);
+Route::get('products/category/{categoryId}', [ProductController::class, 'getByCategory']);
+Route::get('products/search', [ProductController::class, 'search']);
+Route::patch('products/{id}/toggle-status', [ProductController::class, 'toggleStatus']);
