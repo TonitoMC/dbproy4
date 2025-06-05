037bdc2c0edbc5cc0d3733a3f44e70357c14fa8f
DijanU <pad23663@uvg.edu.gt>
2025-06-05 00:57:53 -0600

    add: routes for suppliers and categories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,3 +18,12 @@ Route::get('products/brand/{brandId}', [ProductController::class, 'getByBrand'])
 Route::get('products/category/{categoryId}', [ProductController::class, 'getByCategory']);
 Route::get('products/search', [ProductController::class, 'search']);
 Route::patch('products/{id}/toggle-status', [ProductController::class, 'toggleStatus']);
+
+// Gestión de suppliers
+Route::post('products/{id}/suppliers', [ProductController::class, 'attachSuppliers']);
+Route::delete('products/{id}/suppliers', [ProductController::class, 'detachSuppliers']);
+Route::patch('products/{id}/suppliers/{supplierId}', [ProductController::class, 'updateSupplier']);
+
+// Gestión de categories  
+Route::post('products/{id}/categories', [ProductController::class, 'attachCategories']);
+Route::delete('products/{id}/categories', [ProductController::class, 'detachCategories']);
